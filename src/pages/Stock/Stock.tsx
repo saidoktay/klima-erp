d43fe914cb87ea35d4ead13e6433656e545c0c58
Said Oktay <saidoktay07@gmail.com>
2026-02-02 19:30:14 +0300

    Stok sayfası oluşturuldu

---
+++ src/pages/Stock/Stock.tsx
@@ -0,0 +1,5 @@
+import WareBoard from "./Ware/WareBoard";
+import "./Stock.css";
+export const Stock = () => {
+  return <WareBoard />;
+};
---
+++ src/pages/Stock/Ware/WareBoard.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const WareBoard = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default WareBoard
